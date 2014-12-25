a4942369146e15083ab77ad3cdead09b9953b8aa
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-25 10:57:07 +0530

    added api for finding common trends between 2 places

--- twitter/auth_twitter.py
+++ twitter/auth_twitter.py
@@ -1,4 +1,5 @@
 import twitter
+import json
 
 
 def authorize():
@@ -18,17 +19,38 @@ def authorize():
 
 def getTrending(tapi, woeid):
     trends = tapi.trends.place(_id=woeid)
-    print trends
+    return trends
+
+
+def commonTrends(first, second):
+    first_set = set([trend['name'] for trend in first[0]['trends']])
+    second_set = set([trend['name'] for trend in second[0]['trends']])
+
+    common = first_set.intersection(second_set)
+    return common
+
+
+def prettyOutput(trends):
+    print json.dumps(trends, indent=1)
 
 
 def main():
     tapi = authorize()
-    BLORE_WOEID = 23424848
-    getTrending(tapi, BLORE_WOEID)
-    WORLD_WOEID = 1
-    getTrending(tapi, WORLD_WOEID)
+    IND_WOEID = 23424848
     US_WOEID = 23424977
-    getTrending(tapi, US_WOEID)
+    WORLD_WOEID = 1
+
+    indTrend = getTrending(tapi, IND_WOEID)
+    worldTrend = getTrending(tapi, WORLD_WOEID)
+    usTrend = getTrending(tapi, US_WOEID)
+
+    common_trend = commonTrends(indTrend, usTrend)
+    print "Common Trending between India and US: "
+    print common_trend
+
+    print "Common Trends between US and World: "
+    common_trend = commonTrends(worldTrend, usTrend)
+    print common_trend
 
 if __name__ == '__main__':
     main()
